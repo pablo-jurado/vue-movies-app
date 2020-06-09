752c0c836874aabb9e125057d6735574ef4602f1
Pablo Jurado <pjurado85@gmail.com>
2020-06-09 14:21:51 -0500

    add intersection observer to movie list component

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="flex flex-wrap">
-    <MovieCard
-      v-for="item in movies"
-      :key="item.imdbID"
-      :id="item.imdbID"
-      :title="item.Title"
-      :year="item.Year"
-      :src="item.Poster"
-    />
+  <div v-show="movies.length !== 0">
+    <div class="flex flex-wrap">
+      <MovieCard
+        v-for="item in movies"
+        :key="item.imdbID"
+        :id="item.imdbID"
+        :title="item.Title"
+        :year="item.Year"
+        :src="item.Poster"
+      />
+    </div>
+    <div id="scroll-track" ref="scroll">Scroll</div>
   </div>
 </template>
 
@@ -17,6 +20,28 @@ import MovieCard from "./MovieCard.vue";
 export default {
   components: {
     MovieCard,
+  },
+  data() {
+    return {
+      observer: null,
+    };
+  },
+  mounted() {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0,
+    };
+
+    const callback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) this.$store.commit("updatePageNumber");
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.scroll);
+    this.observer = observer;
   },
   props: {
     movies: Array,
--- src/store.js
+++ src/store.js
@@ -6,10 +6,15 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {
     movies: [],
+    pageNumber: 0,
   },
   mutations: {
     updateMovies(state, value) {
       state.movies.push(...value);
+    },
+    updatePageNumber(state) {
+      console.log(state);
+      state.pageNumber++;
     },
     resetMovies(state) {
       state.movies = [];
