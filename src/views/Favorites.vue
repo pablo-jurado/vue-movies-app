eec26dd01023f6df2c57304e1740116184175b8e
Pablo Jurado <pjurado85@gmail.com>
2020-06-10 15:05:23 -0500

    update favorites styles

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -2,10 +2,13 @@
   <div>
     <h1 class="text-xl font-bold">Movies saved: {{ $store.state.favorites.length }}</h1>
     <div
-      class="m-8 inline-block my-4 border border-gray-200 rounded shadow"
+      class="wrapper m-8 inline-block my-4 border border-gray-200 rounded shadow"
       v-for="(movie) of $store.state.favorites"
       :key="movie.id"
     >
+      <button
+        class="bg-red-400 hover:bg-red-500 text-white font-bold rounded-full px-2 py-0 m-2 float-right"
+      >x</button>
       <p class="p-4">{{movie.title}}</p>
       <img :src="movie.poster" :alt="`poster of ${movie.title}`" />
     </div>
@@ -13,6 +16,11 @@
 </template>
 
 <style scoped>
+.wrapper {
+  max-width: 250px;
+  max-height: 450px;
+  overflow: hidden;
+}
 img {
   max-height: 400px;
 }
