86202f943a38d35a7e7501d7e8fbb02385031d84
Pablo Jurado <pjurado85@gmail.com>
2020-06-10 14:53:38 -0500

    update favorites component

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <h1>Favorites comming soon</h1>
+    <h1 class="text-xl font-bold">Movies saved: {{ $store.state.favorites.length }}</h1>
+    <div
+      class="m-8 inline-block my-4 border border-gray-200 rounded shadow"
+      v-for="(movie) of $store.state.favorites"
+      :key="movie.id"
+    >
+      <p class="p-4">{{movie.title}}</p>
+      <img :src="movie.poster" :alt="`poster of ${movie.title}`" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+img {
+  max-height: 400px;
+}
+</style>
