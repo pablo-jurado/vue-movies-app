e4ba785bb6e6ca53fc499d8ef2b00e2f9921f868
Pablo Jurado <pjurado85@gmail.com>
2020-06-11 14:38:17 -0500

    create movie resolver by id

--- server/src/gql/resolvers.js
+++ server/src/gql/resolvers.js
@@ -1,8 +1,25 @@
-const { books } = require("../db");
+const axios = require("axios");
+const { API_URL } = require("../config");
+
+const movieResolver = async (_, args) => {
+  const movieId = args.id;
+  try {
+    const response = await axios.get(API_URL + "&i=" + movieId);
+    if (response.data.Response === "True") {
+      return response.data;
+    } else {
+      console.log(response.Error);
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 
 const resolvers = {
   Query: {
-    books: () => books,
+    movie: movieResolver,
   },
 };
 
