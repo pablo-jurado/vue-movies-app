cd4b1431dee980244eae7163493679d2276275cb
Pablo Jurado <pjurado85@gmail.com>
2020-06-11 11:00:11 -0500

    install express

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,8 @@
-console.log("running...");
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 8081;
+
+app.listen(port, () => {
+  console.log(`running on port: ${port}`);
+});
