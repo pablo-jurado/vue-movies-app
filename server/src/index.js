aabe4901b3afb3889250f30bca9238e358785e19
Pablo Jurado <pjurado85@gmail.com>
2020-06-11 12:11:40 -0500

    setup apollo server

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const app = express();
-
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs } = require("./gql/schema");
+const { resolvers } = require("./gql/resolvers");
 const port = process.env.PORT || 8081;
 
+const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+
+server.applyMiddleware({ app });
+
 app.listen(port, () => {
-  console.log(`running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
+  console.log(`graphql on: http://localhost:${port}${server.graphqlPath}`);
 });
