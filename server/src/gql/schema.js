04e1f1623fcd84bea90c774b9e321ecd7fee0e11
Pablo Jurado <pjurado85@gmail.com>
2020-06-11 14:37:35 -0500

    create movie type

--- server/src/gql/schema.js
+++ server/src/gql/schema.js
@@ -6,8 +6,37 @@ const typeDefs = gql`
     author: String
   }
 
+  type Movie {
+    Title: String
+    Year: String
+    Rated: String
+    Released: String
+    Runtime: String
+    Genre: String
+    Director: String
+    Writer: String
+    Actors: String
+    Plot: String
+    Language: String
+    Country: String
+    Awards: String
+    Poster: String
+    Ratings: String
+    Metascore: String
+    imdbRating: String
+    imdbVotes: String
+    imdbID: String
+    Type: String
+    DVD: String
+    BoxOffice: String
+    Production: String
+    Website: String
+    Response: String
+  }
+
   type Query {
-    books: [Book]
+    movies: [Movie]
+    movie(id: ID!): Movie
   }
 `;
 
